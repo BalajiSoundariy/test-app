4d2722eea7b296ee40275b25e049b8fe4b2bd06d
Technical_Team_Buzzwallet <tech@buzzwallet.in>
2022-01-20 17:37:02 +0530

    Title has been changed again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Swipewire
+                    SWIPEWIRE
                 </div>
 
                 <div class="links">
